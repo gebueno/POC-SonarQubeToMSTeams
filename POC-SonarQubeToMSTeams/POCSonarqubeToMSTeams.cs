042cf9e754575d2cf0ae8bf260cd9a2d2f8f829a
Gerson Bueno <gerson.bueno@clearsale.com.br>
2021-11-26 19:22:46 -0300

    inicio

--- POC-SonarQubeToMSTeams/POCSonarqubeToMSTeams.cs
+++ POC-SonarQubeToMSTeams/POCSonarqubeToMSTeams.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using POC_SonarQubeToMSTeams.Business;
+using POC_SonarQubeToMSTeams.Validator.Interfaces;
 using System;
 using System.IO;
 using System.Net.Http;
@@ -20,18 +21,15 @@ namespace POC_SonarQubeToMSTeams
         private readonly string _sonarqubeWebhookSecret;
         private readonly string _disableAuthentication;
         private readonly string _culture;
-
-        public const string Setting_QualityGateStatusExcludeList = "QualityGateStatusExcludeList";
-
+        
         private readonly HttpClient _httpClient;
         private readonly ISonarqubeToMSTeamsConverter _sonarqubeToMSTeamsConverter;
-        private readonly ISonarqubeToMSTeamsFilter _sonarqubeToMSTeamsFilter;
-        public POCSonarqubeToMSTeams(HttpClient httpClient, ISonarqubeToMSTeamsConverter sonarqubeToMSTeamsConverter, ISonarqubeToMSTeamsFilter sonarqubeToMSTeamsFilter,
-            IConfiguration configuration)
+        private readonly ISonarqubeSecretValidator _sonarqubeSecretValidator;
+
+        public POCSonarqubeToMSTeams(HttpClient httpClient, ISonarqubeToMSTeamsConverter sonarqubeToMSTeamsConverter, IConfiguration configuration)
         {
             _httpClient = httpClient;
             _sonarqubeToMSTeamsConverter = sonarqubeToMSTeamsConverter;
-            _sonarqubeToMSTeamsFilter = sonarqubeToMSTeamsFilter;
             _teamsWebhookUrl = configuration["TeamsWebhookUrl"];
             _disableAuthentication = configuration["DisableAuthentication"];
             _sonarqubeWebhookSecret = configuration["SonarqubeWebhookSecret"];
@@ -51,46 +49,36 @@ namespace POC_SonarQubeToMSTeams
             // Get and validate config settings
             // ----------------------------------------------------
             // Required setting: SonarqubeWebhookSecret. Contains the Sonarqube Webhook secret. It's the same secret that was configured in the Sonarqube Webhook.
-            //if(string.IsNullOrEmpty(_sonarqubeWebhookSecret))
-            //{
-            //    log.LogError($"Required setting {SonarqubeWebhookSecret is missing.");
-            //    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
-            //}
+            if (string.IsNullOrEmpty(_sonarqubeWebhookSecret))
+            {
+                log.LogError($"Required setting SonarqubeWebhookSecret is missing.");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
 
             // Required setting: TeamsWebhookUrl
-            //var teamsWebhookUrl = Environment.GetEnvironmentVariable(_teamsUrl, EnvironmentVariableTarget.Process);
             if (string.IsNullOrEmpty(_teamsWebhookUrl))
             {
                 log.LogError($"Required setting {_teamsWebhookUrl} is missing.");
                 return new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
 
-            // Optional setting: QualityGateStatusExcludeList. A comma separated list of Sonarqube Quality Status values that should be ignored.
-            var qualityGateStatusExcludes = Environment.GetEnvironmentVariable(Setting_QualityGateStatusExcludeList, EnvironmentVariableTarget.Process);
-
-            // Optional setting: Culture. Affects displayed Date/time values in MS Teams card. Examples: en-US, sv-SE. If not specified, dates/times are displayed as received from Sonarqube
-            var culture = Environment.GetEnvironmentVariable(_culture, EnvironmentVariableTarget.Process);
+            // Optional setting: DisableAuthentication. Disables validation of Sonarqube Webhook secret. Use for local development only!
+            bool disableAuthentication = !string.IsNullOrEmpty(_disableAuthentication) && _disableAuthentication.Equals("true", StringComparison.CurrentCultureIgnoreCase);
 
             // ----------------------------------------------------
             // Validate signature in http header
             // ----------------------------------------------------
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //if(!disableAuthentication && !_sonarqubeSecretValidator.IsValidSignature(req, requestBody, sonarqubeWebhookSecret))
-            //{
-            //    log.LogWarning($"Sonarqube secret http header is missing or not valid. Config setting {Setting_SonarqubeWebhookSecret} must match secret in Sonarqube Webhook header {SonarqubeSecretValidator.SonarqubeAuthSignatureHeaderName}.");
-            //    return new UnauthorizedResult();
-            //}
+            if (!disableAuthentication && !_sonarqubeSecretValidator.IsValidSignature(req, requestBody, _sonarqubeWebhookSecret))
+            {
+                log.LogWarning($"Sonarqube secret http header is missing or not valid. Config setting SonarqubeWebhookSecret must match secret in Sonarqube Webhook header.");
+                return new UnauthorizedResult();
+            }
 
             // ----------------------------------------------------
             // Check if a card should be sent to MS Teams or not.
             // ----------------------------------------------------
-            dynamic sonarqubeRequestJson = JsonConvert.DeserializeObject(requestBody);
-
-            if (!_sonarqubeToMSTeamsFilter.ShouldProcess(sonarqubeRequestJson, qualityGateStatusExcludes))
-            {
-                log.LogInformation($"Message was not sent to MS Teams due to filter.");
-                return new OkResult();
-            }
+            dynamic sonarqubeRequestJson = JsonConvert.DeserializeObject(requestBody);            
 
             // ----------------------------------------------------
             // Build MS Teams card from Sonarqube Webhook data 
